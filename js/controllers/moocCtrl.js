1b8bb657bb1504b01f25194cfdde1f59f8b7ffb7
Steven Hein <steven.c.hein@gmail.com>
2015-09-22 16:55:01 -0400

    added the slider and some other things

--- js/controllers/moocCtrl.js
+++ js/controllers/moocCtrl.js
@@ -3,6 +3,11 @@
 angular.module('mooc')
 	.controller('moocCtrl', function moocCtrl($scope, $routeParams, $filter) {
 		'use strict';
-		
+
+		$('#slider').slider({
+			formatter: function(value) {
+				return 'Current value: ' + value;
+			}
+		});
 		
 	});
